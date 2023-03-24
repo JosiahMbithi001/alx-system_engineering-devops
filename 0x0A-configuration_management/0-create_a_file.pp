f18e48e5248494ee6f971ec7e4993f74ec35fc8f
JosiahMbithi001 <mbithishucks@gmail.com>
2023-03-24 04:16:59 -0700

    All Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This Manifest creates a File in /tmp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   =>  'www-data',
+  group   => 'www-data',
+}
