69975688ff5303fcaa0fbc1cf9ec666783570a38
JosiahMbithi001 <mbithishucks@gmail.com>
2023-05-17 11:53:17 -0700

    sky isn't the Limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# This puppet script increases the amount of traffic nginx cans serve
+
+# Increase the ULIMIT of the default file
+
+exec { 'fix--for-nginx':
+	command => "sed-i 's/15/4096/' /etc/default/nginx",
+	path => "/usr/local/bin/:/bin/"
+}
+
+# Restart nginx
+
+exec { 'nginx-restart':
+	command => "nginx restart",
+	path => '/etc/init.d'
+}
